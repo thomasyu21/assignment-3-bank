f5eea3ceaabf4b111296a0a5f55192dfa975a767
thomasyu21 <thomasyu085@gmail.com>
2024-11-01 21:57:29 -0400

    credits page and removed toFixed from new debit amounts

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,33 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+  let creditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {
+      let date = credit.date.slice(0,10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    });
+  }
+
   return (
     <div>
       <h1>Credits</h1>
+      <AccountBalance accountBalance={props.accountBalance} />
+      <br/>
+
+      {creditsView()}
+
+      <h4>Add Credit</h4>
+      <form onSubmit={props.addCredit}>
+        Description:<input type="text" name="description" />
+        <br/>
+        Amount:<input type="number" step="0.01" name="amount" />
+        <br/>
+        <button type="submit">Add Credit</button>
+      </form>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
