3317c5de4a3a3b3af2b8f4fe6d0c6ff1aadaa1c9
thomasyu21 <thomasyu085@gmail.com>
2024-11-01 20:26:11 -0400

    API call, credit and debit lists, and account balance updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ It contains the top-level state.
 ==================================================*/
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
 
 // Import other components
 import Home from './components/Home';
@@ -33,6 +34,37 @@ class App extends Component {
     const newUser = {...this.state.currentUser};
     newUser.userName = logInInfo.userName;
     this.setState({currentUser: newUser})
+  }
+
+  async componentDidMount() {
+    let creditAPI = 'https://johnnylaicode.github.io/api/credits.json';  // Link to API for credits
+    let debitAPI = 'https://johnnylaicode.github.io/api/debits.json';  // Link to API for debits
+
+    // Await for promise (completion) returned from API call
+    try {
+      // Set Credit and Debit Lists
+      let creditResponse = await axios.get(creditAPI); // JSON object for credits
+      this.setState({creditList: creditResponse.data}); // Set creditList
+      //console.log(this.state.creditList);
+
+      let debitResponse = await axios.get(debitAPI); // JSON object for debits
+      this.setState({debitList: debitResponse.data}); // Set debitList
+      //console.log(this.state.debitList);
+
+      // Set Balance
+      let balance = 0
+      this.state.creditList.forEach((credit) => balance+=credit.amount)
+      this.state.debitList.forEach((debit) => balance-=debit.amount)
+      this.setState({accountBalance: Math.round(balance*100)/100}) // Round to 2 decimal points
+      //console.log(this.state.accountBalance)
+    } 
+    catch (error) {  // Print out errors at console when there is an error response
+      if (error.response) {
+        // The request was made, and the server responded with error message and status code.
+        console.log(error.response.data);  // Print out error message (e.g., Not Found)
+        console.log(error.response.status);  // Print out error status code (e.g., 404)
+      }    
+    }
   }
 
   // Create Routes and React elements to be rendered using React components
